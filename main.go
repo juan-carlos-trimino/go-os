510404e0372902ce54ced39d116fe9f62182e373
Juan Carlos Trimiño <juancarlos@trimino.com>
2025-05-18 00:02:09 -0500

    add a comment.

--- main.go
+++ main.go
@@ -29,46 +29,47 @@ Next, open the file that will use the package and add this line
 
 import "github.com/juan-carlos-trimino/gposu"
 
-To upgrade/downgrade the version of the package, execute
+To upgrade/downgrade the version of the package, move to the root of the module's directory
+structure (where the go.mod file is located) and execute
 (go get -u "{package-name}@{git-commit-hash}").
 $ go get -u "github.com/juan-carlos-trimino/gposu@b33734a"
 ***/
 
 import (
-  "bufio"
-  "io"
-  "os"
-  "os/user"
-  "runtime"
-  "strings"
-  "sync"
-  "syscall"
+	"bufio"
+	"io"
+	"os"
+	"os/user"
+	"runtime"
+	"strings"
+	"sync"
+	"syscall"
 )
 
 // Grouping together three related variables in a single package-level variable, protect.
-var shr = struct {  //Unnamed struct.
-  mutexDirs sync.Mutex //Protect the file.
+var shr = struct { //Unnamed struct.
+	mutexDirs sync.Mutex //Protect the file.
 }{}
 
 // Is the current user running as root?
 func IsRoot() (bool, error) {
-  current, err := user.Current()
-  if err != nil {
-    return false, err
-  }
-  return strings.EqualFold(current.Username, "root"), nil
+	current, err := user.Current()
+	if err != nil {
+		return false, err
+	}
+	return strings.EqualFold(current.Username, "root"), nil
 }
 
 func GetUsername() (string, error) {
-  current, err := user.Current()
-  if err != nil {
-    return "", err
-  }
-  return current.Username, nil
+	current, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	return current.Username, nil
 }
 
 func GetOS() string {
-  return runtime.GOOS
+	return runtime.GOOS
 }
 
 /*
@@ -79,11 +80,11 @@ Go populates this variable by quering the OS to determine how many CPUs the syst
 **
 */
 func CpusAvailable() (numOfCpus, maxProcs int) {
-  //Go defaults the value of GOMAXPROCS to the value of NumCPU().
-  numOfCpus = runtime.NumCPU() //Number of CPUs.
-  //Calling GOMAXPROCS(n) with n < 1 returns the current value without altering it.
-  maxProcs = runtime.GOMAXPROCS(0)
-  return
+	//Go defaults the value of GOMAXPROCS to the value of NumCPU().
+	numOfCpus = runtime.NumCPU() //Number of CPUs.
+	//Calling GOMAXPROCS(n) with n < 1 returns the current value without altering it.
+	maxProcs = runtime.GOMAXPROCS(0)
+	return
 }
 
 /*
@@ -110,15 +111,15 @@ Permission  Effect on files                     Effective on directories
 ---------------------------------------------------------------------------------------------------
 r (read)    File contents can be read.          Contents of the directory (the file names) can be
 
-  listed. Octal value of 4.
+	listed. Octal value of 4.
 
 w (write)   File contents can be changed.       Any file in the directory can be created, deleted,
 
-  or renamed. Octal value of 2.
+	or renamed. Octal value of 2.
 
 x (execute) Files can be executed as commands.  The directory can become the current working
 
-  directory. Octal value of 1.
+	directory. Octal value of 1.
 
 The umask command without arguments will display the current value of the shell's umask:
 $ umask
@@ -139,138 +140,138 @@ $ ls -ld directory_name
 **
 */
 func CreateDirs(umask int, perm os.FileMode, dirs ...string) (string, error) {
-  sb := strings.Builder{}
-  //Grow to a larger size to reduce future resizes of the buffer.
-  sb.Grow(1024)
-  shr.mutexDirs.Lock()
-  defer shr.mutexDirs.Unlock()
-  oldMask := syscall.Umask(umask)
-  defer syscall.Umask(oldMask)
-  for _, dir := range dirs {
-    sb.WriteString(dir)
-    if !strings.HasSuffix(sb.String(), "/") {
-      sb.WriteString("/")
-    }
-    //
-    if _, err := os.Stat(sb.String()); err != nil {
-      if os.IsNotExist(err) {
-        err := os.Mkdir(sb.String(), perm)
-        if err != nil {
-          return sb.String(), err
-        }
-      } else {
-        return "", err
-      }
-    }
-  }
-  return sb.String(), nil
+	sb := strings.Builder{}
+	//Grow to a larger size to reduce future resizes of the buffer.
+	sb.Grow(1024)
+	shr.mutexDirs.Lock()
+	defer shr.mutexDirs.Unlock()
+	oldMask := syscall.Umask(umask)
+	defer syscall.Umask(oldMask)
+	for _, dir := range dirs {
+		sb.WriteString(dir)
+		if !strings.HasSuffix(sb.String(), "/") {
+			sb.WriteString("/")
+		}
+		//
+		if _, err := os.Stat(sb.String()); err != nil {
+			if os.IsNotExist(err) {
+				err := os.Mkdir(sb.String(), perm)
+				if err != nil {
+					return sb.String(), err
+				}
+			} else {
+				return "", err
+			}
+		}
+	}
+	return sb.String(), nil
 }
 
 func ReadAllShareLock1(filePath string, flag int, perm os.FileMode) ([]byte, error) {
-  file, err := os.OpenFile(filePath, flag, perm)
-  if err != nil {
-    return nil, err
-  }
-  //Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
-  //are executed in last-in-first-out order.
-  defer file.Close()
-  if err := syscall.Flock(int(file.Fd()), syscall.LOCK_SH); err != nil { //Share reads.
-    return nil, err
-  }
-  defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
-  obj, err := io.ReadAll(file)
-  if err != nil {
-    return nil, err
-  }
-  return obj, nil
+	file, err := os.OpenFile(filePath, flag, perm)
+	if err != nil {
+		return nil, err
+	}
+	//Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
+	//are executed in last-in-first-out order.
+	defer file.Close()
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_SH); err != nil { //Share reads.
+		return nil, err
+	}
+	defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
+	obj, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	return obj, nil
 }
 
 func ReadAllShareLock2(filePath string, data map[string][]byte, flag int, perm os.FileMode) error {
-  file, err := os.OpenFile(filePath, flag, perm)
-  if err != nil {
-    return err
-  }
-  //Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
-  //are executed in last-in-first-out order.
-  defer file.Close()
-  builder := strings.Builder{}
-  //Grow to a larger size to reduce future resizes of the buffer.
-  builder.Grow(1024)
-  if err := syscall.Flock(int(file.Fd()), syscall.LOCK_SH); err != nil { //Share reads.
-    return err
-  }
-  defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
-  scanner := bufio.NewScanner(file)
-  for scanner.Scan() {
-    if builder.Len() == 0 {
-      builder.WriteString(scanner.Text())
-    } else {
-      data[builder.String()] = scanner.Bytes()
-      builder.Reset()
-    }
-  }
-  return nil
+	file, err := os.OpenFile(filePath, flag, perm)
+	if err != nil {
+		return err
+	}
+	//Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
+	//are executed in last-in-first-out order.
+	defer file.Close()
+	builder := strings.Builder{}
+	//Grow to a larger size to reduce future resizes of the buffer.
+	builder.Grow(1024)
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_SH); err != nil { //Share reads.
+		return err
+	}
+	defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if builder.Len() == 0 {
+			builder.WriteString(scanner.Text())
+		} else {
+			data[builder.String()] = scanner.Bytes()
+			builder.Reset()
+		}
+	}
+	return nil
 }
 
 func WriteAllExclusiveLock1(filePath string, data []byte, flag int, perm os.FileMode) (int, error) {
-  file, err := os.OpenFile(filePath, flag, perm)
-  if err != nil {
-    return -1, err
-  }
-  //Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
-  //are executed in last-in-first-out order.
-  defer file.Close()
-  if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil { //Exclusive write.
-    return -1, err
-  }
-  defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
-  n, err := file.Write(data)
-  if err != nil {
-    return -1, err
-  }
-  return n, nil
+	file, err := os.OpenFile(filePath, flag, perm)
+	if err != nil {
+		return -1, err
+	}
+	//Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
+	//are executed in last-in-first-out order.
+	defer file.Close()
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil { //Exclusive write.
+		return -1, err
+	}
+	defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
+	n, err := file.Write(data)
+	if err != nil {
+		return -1, err
+	}
+	return n, nil
 }
 
 func WriteAllExclusiveLock2(filePath string, data1 string, data2 []byte, flag int,
-  perm os.FileMode) (int, int, error) {
-  file, err := os.OpenFile(filePath, flag, perm)
-  if err != nil {
-    return -1, -1, err
-  }
-  //Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
-  //are executed in last-in-first-out order.
-  defer file.Close()
-  if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil { //Exclusive write.
-    return -1, -1, err
-  }
-  defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
-  var n1, n2 int = -1, -1
-  if n1, err = file.WriteString(data1); err == nil {
-    n2, err = file.Write(data2)
-  }
-  return n1, n2, err
+	perm os.FileMode) (int, int, error) {
+	file, err := os.OpenFile(filePath, flag, perm)
+	if err != nil {
+		return -1, -1, err
+	}
+	//Deferred function calls are pushed onto a stack. When a function returns, its deferred calls
+	//are executed in last-in-first-out order.
+	defer file.Close()
+	if err := syscall.Flock(int(file.Fd()), syscall.LOCK_EX); err != nil { //Exclusive write.
+		return -1, -1, err
+	}
+	defer syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
+	var n1, n2 int = -1, -1
+	if n1, err = file.WriteString(data1); err == nil {
+		n2, err = file.Write(data2)
+	}
+	return n1, n2, err
 }
 
 func CheckFileExists(fileName string) (bool, error) {
-  info, err := os.Stat(fileName)
-  if err != nil {
-    if os.IsNotExist(err) {
-      return false, nil
-    } else {
-      return false, err
-    }
-  }
-  return !info.IsDir(), nil
+	info, err := os.Stat(fileName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+	return !info.IsDir(), nil
 }
 
 func CheckDirExists(dirName string) (bool, error) {
-  info, err := os.Stat(dirName)
-  if err != nil {
-    if os.IsNotExist(err) {
-      return false, nil
-    } else {
-      return false, err
-    }
-  }
-  return info.IsDir(), nil
+	info, err := os.Stat(dirName)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	}
+	return info.IsDir(), nil
 }
